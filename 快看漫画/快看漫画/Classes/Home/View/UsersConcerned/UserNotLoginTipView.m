d8ac4471b81ce11ecea5d6760e1f1457b7899286
金亮齐 <15914115336@163.com>
2016-12-13 22:27:47 +0800

    Home
    HomeConotroller首页视图

---
+++ 快看漫画/快看漫画/Classes/Home/View/UsersConcerned/UserNotLoginTipView.m
@@ -0,0 +1,21 @@
+//
+//  UserNotLoginTipView.m
+//  快看漫画
+//
+//  Created by 金亮齐 on 2016/12/13.
+//  Copyright © 2016年 醉看红尘这场梦. All rights reserved.
+//
+
+#import "UserNotLoginTipView.h"
+
+@implementation UserNotLoginTipView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
