46bbb2a39618b82ef4d89d7d375e525ea42f2c3c
金亮齐 <15914115336@163.com>
2016-12-14 23:32:20 +0800

    AFN错误处理方法
    2.在targets->build
    phases中修改compilerFlags属性，添加：-fno-objc-arc，就可以让原来支持arc的工程不使用arc，对于大部分第三方类
    库来说都可以顺利编译通过

--- 快看漫画/快看漫画/Classes/Managers/NetWorkManager.h
+++ 快看漫画/快看漫画/Classes/Managers/NetWorkManager.h
@@ -5,7 +5,8 @@
 //  Created by 金亮齐 on 2016/12/13.
 //  Copyright © 2016年 醉看红尘这场梦. All rights reserved.
 //
-#import <AFNetworking.h>
+#import <Foundation/Foundation.h>
+#import "AFNetworking.h"
 
 static NSString * const NetCode     = @"code";
 static NSString * const NetOk       = @"OK";
