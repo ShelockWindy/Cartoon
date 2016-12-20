48fb2fb533b28e822ba3af04d0290905025892ac
金亮齐 <15914115336@163.com>
2016-12-20 22:42:20 +0800

    作品详情
    添加作品详情数据

---
+++ 快看漫画/快看漫画/Classes/PublicView/WorksDetail(作品详情)/View/wordDescSectionHeadView.m
@@ -0,0 +1,21 @@
+//
+//  wordDescSectionHeadView.m
+//  快看漫画
+//
+//  Created by 金亮齐 on 2016/12/20.
+//  Copyright © 2016年 醉看红尘这场梦. All rights reserved.
+//
+
+#import "wordDescSectionHeadView.h"
+
+@implementation wordDescSectionHeadView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
---
+++ 快看漫画/快看漫画/Classes/PublicView/WorksDetail(作品详情)/View/wordsSequenceView.m
@@ -0,0 +1,21 @@
+//
+//  wordsSequenceView.m
+//  快看漫画
+//
+//  Created by 金亮齐 on 2016/12/20.
+//  Copyright © 2016年 醉看红尘这场梦. All rights reserved.
+//
+
+#import "wordsSequenceView.h"
+
+@implementation wordsSequenceView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
